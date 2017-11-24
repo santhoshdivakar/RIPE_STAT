aa396bef091ea19d82ea22e60bcb3b4769c8947e
santhosh.divakar <santhosh.divakar@ril.com>
2017-11-25 03:16:58 +0530

    Removing json2xml

--- ripeutil/ripe_util.py
+++ ripeutil/ripe_util.py
@@ -3,21 +3,20 @@ Doc String
 """
 import argparse
 import re
-import requests
-import dicttoxml
 import json
 import yaml
-from src.json2xml import Json2xml
+import requests
+import dicttoxml
 
 SUPPORTED_FORMAT_TYPES = ['json', 'yaml', 'xml']
 SUPPORTED_ACTION_TYPES = ['network-info', 'geoloc', 'as-overview']
 SOFT_LIMIT_IGNORE = 'soft_limit=ignore'
-ACTION_PARAM_MAP = {'network-info' : ['ip'], 'geoloc' : ['ip', 'asn'], 'as-overview' : ['asn'] }
+ACTION_PARAM_MAP = {'network-info' : ['ip'], 'geoloc' : ['ip', 'asn'], 'as-overview' : ['asn']}
 
-def _check_ip_type(ip):
-    if re.match("""^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$""", ip):
+def _check_ip_type(ip_addr):
+    if re.match("""^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$""", ip_addr): #pylint: disable=C0301
         return 'ipv4'
-    #if re.match("""(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])(\/\d{1,3})?)""", ip):
+    #if re.match("""(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])(\/\d{1,3})?)""", ip): #pylint: disable=C0301
     #    return 'ipv6'
     #Check for the IPV6 before
     return 'none'
@@ -43,7 +42,7 @@ def _result_format(fmt, data):
         return yaml.dump(data)
     return data
 
-def _validate_inputs(action, fmt, ip_addr, asn_list):
+def _validate_inputs(action, fmt, ip_addr_list, asn_list):
     if fmt not in SUPPORTED_FORMAT_TYPES:
         return {'status':'nok', 'error':'invalid output format'}
     if action not in SUPPORTED_ACTION_TYPES:
@@ -51,11 +50,11 @@ def _validate_inputs(action, fmt, ip_addr, asn_list):
 
     param_list = []
     if 'ip' in ACTION_PARAM_MAP[action]:
-        for ip in ip_addr:
-            if _check_ip_type(ip) == 'none':
-                return {'status':'nok', 'error':'invalid IP %s'%(ip,)}
+        for ip_addr in ip_addr_list:
+            if _check_ip_type(ip_addr) == 'none':
+                return {'status':'nok', 'error':'invalid IP %s'%(ip_addr,)}
             else:
-                param_list.append(ip)
+                param_list.append(ip_addr)
             pass
         pass
     pass
@@ -68,9 +67,8 @@ def _validate_inputs(action, fmt, ip_addr, asn_list):
             pass
         pass
     pass
-    if len(param_list) == 0:
+    if not param_list:
         return {'status':'nok', 'error':'no valid parameters provided for the action'}
-    pass
 
     return {'status':'ok', 'data': param_list}
 
@@ -86,8 +84,8 @@ def get_ripe_stat(action='network-info', fmt='json', ipaddr=[], asn_list=[]):
         return _result_format(fmt, validate_inputs)
     else:
         param_list = validate_inputs['data']
-    
-    # Form the URL parameters and call the service. 
+
+    # Form the URL parameters and call the service.
     url = 'https://stat.ripe.net/data/%s/data.json?resource='%(action,)
 
     # Call the service.since any input has the same key, we use a single param_list
@@ -104,20 +102,23 @@ def get_ripe_stat(action='network-info', fmt='json', ipaddr=[], asn_list=[]):
     # Parse the reply to the format we need
     return _result_format(fmt, {'status':'ok', 'data':data})
 
-def main():
+def _main():
+    """
+    The Main function to be called when the module is invoked as command line
+    """
     my_parser = argparse.ArgumentParser(description='RIPE Statistics Tool')
     my_parser.add_argument(
-        '-a','--action', type=str, 
-        choices=SUPPORTED_ACTION_TYPES, 
+        '-a', '--action', type=str,
+        choices=SUPPORTED_ACTION_TYPES,
         default='network-info')
-    my_parser.add_argument('-i','--ipaddr', type=str, action='append', default=[])
+    my_parser.add_argument('-i', '--ipaddr', type=str, action='append', default=[])
     my_parser.add_argument('--asn', type=str, action='append', default=[])
-    my_parser.add_argument('-f','--format', type=str, choices=SUPPORTED_FORMAT_TYPES, default='json')
+    my_parser.add_argument('-f', '--format', type=str, choices=SUPPORTED_FORMAT_TYPES, default='json')
     arg_list = vars(my_parser.parse_args())
     print(get_ripe_stat(action=arg_list['action'], ipaddr=arg_list['ipaddr'], \
                       fmt=arg_list['format'], asn_list=arg_list['asn']))
 
 
 if __name__ == '__main__':
-    main()
+    _main()
 
