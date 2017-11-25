1c9c8ea05c1125b758dda3ae7e0b5781b3c83f5c
santhosh.divakar <santhosh.divakar@ril.com>
2017-11-26 03:56:50 +0530

    Test cases addition

---
+++ ripeutil/tests/test_functionality.py
@@ -0,0 +1,23 @@
+"""
+Module to test the functionality of ripe_util Module.
+Both positive and negative tests will be covered.
+"""
+import sys
+import unittest
+
+sys.path.append('..')
+import ripe_util
+
+class TestStringMethods(unittest.TestCase):
+    """
+    To test the validation of the inputs
+    """
+    def test_network_info(self):
+        pass
+    def test_as_overview(self):
+        pass
+    def test_geoloc(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
