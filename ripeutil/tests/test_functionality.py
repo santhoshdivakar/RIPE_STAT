863d66e3cd961f324f58c18dc076ad75037a2d85
santhosh.divakar <santhosh.divakar@ril.com>
2017-11-29 02:37:45 +0530

    Adding the functionality tests

--- ripeutil/tests/test_functionality.py
+++ ripeutil/tests/test_functionality.py
@@ -13,6 +13,20 @@ class TestStringMethods(unittest.TestCase):
     To test the validation of the inputs
     """
     def test_network_info(self):
+        #ipv4
+        output = ripe_util.get_ripe_stat(action='network-info', fmt='json',ipaddr=['49.44.97.154'])
+        reply = output["data"][0]
+        self.assertEqual(reply['status'],'ok')
+        #ipv6
+        output = ripe_util.get_ripe_stat(action='network-info', fmt='json',ipaddr=['2001:4860:4860::8844'])
+        reply = output["data"][0]
+        self.assertEqual(reply['status'],'ok')
+        #ipv4+ipv6
+        output = ripe_util.get_ripe_stat(action='network-info', fmt='json',ipaddr=['8.8.8.8','2001:4860:4860::8844'])
+        reply = output["data"][0]
+        self.assertEqual(reply['status'],'ok')
+        reply = output["data"][1]
+        self.assertEqual(reply['status'],'ok')
         pass
     def test_as_overview(self):
         pass
