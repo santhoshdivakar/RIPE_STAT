ccb106a431a62039be5221e0360350b086bd489a
santhosh.divakar <santhosh.divakar@ril.com>
2017-12-01 03:10:34 +0530

    README changes for tests

--- ripeutil/tests/test_format.py
+++ ripeutil/tests/test_format.py
@@ -5,6 +5,8 @@ Both positive and negative tests will be covered.
 import sys
 import unittest
 import json
+import yaml
+import xml.etree.ElementTree as ET
 
 sys.path.append('..')
 from ripeutil import ripe_util
@@ -14,10 +16,19 @@ class TestStringMethods(unittest.TestCase):
     To test the output format of the functions
     """
     def test_json_format(self):
+        output = ripe_util.get_ripe_stat(action='geoloc', fmt='json',ipaddr=['2001:4860:4860::8844'])
+        output = json.loads(output)
+        self.assertTrue('status' in output.keys())
         pass
     def test_xml_format(self):
+        output = ripe_util.get_ripe_stat(action='geoloc', fmt='xml',ipaddr=['2001:4860:4860::8844'])
+        root = ET.fromstring(output)
+        self.assertTrue(root[0].tag == 'status')
         pass
     def test_yaml_format(self):
+        output = ripe_util.get_ripe_stat(action='geoloc', fmt='yaml',ipaddr=['2001:4860:4860::8844'])
+        output = yaml.load(output)
+        self.assertTrue('status' in output.keys())
         pass
 
 if __name__ == '__main__':
